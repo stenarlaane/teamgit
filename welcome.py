b395f462ad03ca9e2b39a89a6579bc2b0dba4f58
kris-v <kristjan.kraak@voco.ee>
2025-04-10 09:40:51 +0300

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,7 +1,3 @@
-from get_name import *
-print("="*35)
-print("Welcome to the voting eligibility check")
-print("="*35)
-print("\n")
-user_name = get_name()
-print(f"Hello {user_name}!\n")
+from validate_age import *
+age = int(input("Enter your age: "))
+validate_age(age)
